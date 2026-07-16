41f3e6f019f0fb46cc4b5d0e24530d5c520e0b04
mdfizz655 <info.mdfizz65@gmail.com>
2026-07-16 23:57:47 +0600

    fix postcss and tailwind dependencies

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**',
-      },
-    ],
+/** @type {import('postcss-load-config').Config} */
+const config = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
   },
-  // পেমেন্ট বা অথেনটিকেশনের জন্য যদি অতিরিক্ত কনফিগ লাগে এখানে আসবে
 };
 
-export default nextConfig;
+export default config;
